e7ae3c96f2ebd92e586689f1d8e8973001694c06
Gary Hills <garythills@gmail.com>
2025-06-11 09:26:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from langchain_community.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
-from langchain.chains.question_answering import load_qa_chain
 
 from vectorstore_utils import load_or_build_vectorstore
 
@@ -13,15 +12,13 @@ st.markdown("Ask anything about Tony Bates' *Teaching in a Digital Age*")
 query = st.text_input("💬 Ask a question:")
 
 vectorstore = load_or_build_vectorstore()
-system_prompt = load_system_prompt()
 
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 
 qa_chain = RetrievalQA.from_chain_type(
     llm=llm,
     retriever=vectorstore.as_retriever(),
-    chain_type="stuff",
-    chain_type_kwargs={"prompt": system_prompt}
+    chain_type="stuff"
 )
 
 if query:
