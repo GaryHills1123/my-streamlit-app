678eb83306e427ce524d9a46c31f93040bba0c20
Gary Hills <garythills@gmail.com>
2025-06-15 07:50:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ if query:
         client = ElevenLabs(api_key=os.getenv("ELEVENLABS_API_KEY"))
         audio_generator = client.text_to_speech.convert(
             text=result,
-            voice_id="d5cOKJcJrXgtdgdwXmpu",  # ← my voice
+            voice_id="Aidp5VE7mbCrwcGQUJxB",  # ← my voice
             model_id="eleven_multilingual_v2",
             output_format="mp3_22050_32",
             optimize_streaming_latency=1
